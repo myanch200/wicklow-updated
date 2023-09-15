483c23bd43f1293423993030069323db25703c97
Martin Yanchev <myanch200@gmail.com>
2023-09-15 22:25:12 +0100

    itegration: adding redis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
-
+  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL", nil) }
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
